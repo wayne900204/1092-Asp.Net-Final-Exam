bd61925f531cfcef73ea01fe75af38cb693d60da
邱文也 <wayne900204@gmail.com>
2021-06-24 18:46:16 +0800

    first commit

---
+++ AspNetFinalExam/Controllers/ExamController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNetFinalExam.Controllers
+{
+    public class ExamController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // GET: /<controller>/
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
