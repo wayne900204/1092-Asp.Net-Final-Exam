f89c7ac763aa128b28336c9cac8e2447e9016e69
邱文也 <wayne900204@gmail.com>
2021-06-24 19:51:39 +0800

    新增第 4 題

--- AspNetFinalExam/Controllers/TestController.cs
+++ AspNetFinalExam/Controllers/TestController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using AspNetFinalExam.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,7 +14,46 @@ namespace AspNetFinalExam.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            return View(new TestData());
+        }
+        [HttpPost]
+
+        public IActionResult Index(TestData data)
+        {
+            if (data.Name == "1")
+            {
+                data.Message = "星期一";
+            }
+            else if (data.Name == "2")
+            {
+                data.Message = "星期二";
+            }
+            else if (data.Name == "3")
+            {
+                data.Message = "星期三";
+            }
+            else if (data.Name == "4")
+            {
+                data.Message = "星期四";
+            }
+            else if (data.Name == "5")
+            {
+                data.Message = "星期五";
+            }
+            else if (data.Name == "6")
+            {
+                data.Message = "星期六";
+            }
+            else if (data.Name == "7")
+            {
+                data.Message = "星期七";
+            }
+            else
+            {
+                data.Message = "Error";
+            }
+            return View(data);
+
         }
     }
 }
---
+++ AspNetFinalExam/ViewModels/TestData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace AspNetFinalExam.ViewModels
+{
+    public class TestData
+    {
+        public string Name { get; set; }
+
+        public string Message { get; set; }
+    }
+}
